7e235b8b96c2c56c00b80846b20b07fcad4297bd
unknown <gustavorstroschon@gmail.com>
2026-04-28 01:23:50 -0300

    updt

--- src/components/LocalMaps.jsx
+++ src/components/LocalMaps.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { m } from "framer-motion";
+import { motion } from "framer-motion";
 import { MapPin, Navigation, Map } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
